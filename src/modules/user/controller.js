99176d5b32f5afdab366dff61222801372adff46
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-06-05 10:29:48 +0700

    Bug fix login user.

--- src/modules/user/controller.js
+++ src/modules/user/controller.js
@@ -13,17 +13,20 @@ UserController.getUser = async (req, res, next) => {
   let profile = req.user;
   if (req.params.id) {
     profile = await User.getById(req.params.id);
+    profile = profile ? profile.toJSON() : profile;
   }
 
   if (!profile) {
     const err = new Error('Invalid user');
     return next(err);
   }
-  if (req.route.path === '/user/login') {
+
+  if (req.route.path === '/users/login') {
     const payload = { id_users: profile.id_users };
     profile.token = jwt.sign(payload, jwtOptions.secretOrKey);
   }
-  profile.date = moment(profile.tgl_create_users).format('YYYY');
+  profile.tgl_create_users = moment(profile.tgl_create_users).unix();
+  profile.tglstatus_users = moment(profile.tglstatus_users).unix();
   delete profile.password_users;
   req.resData = {
     status: true,
