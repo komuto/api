5434d79c434fe31cdac1a10e1c7d836a4c806a22
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-06-20 09:06:10 +0700

    KOM-227 : Get brands.

---
+++ src/modules/brand/controller.js
@@ -0,0 +1,17 @@
+import { Brand } from './model';
+
+export const BrandController = {};
+export default { BrandController };
+
+/**
+ * Get brands
+ */
+BrandController.index = async (req, res, next) => {
+  const brands = await Brand.get();
+  req.resData = {
+    status: true,
+    message: 'Brands Data',
+    data: brands,
+  };
+  return next();
+};
---
+++ src/modules/brand/index.js
@@ -0,0 +1,4 @@
+import * as model from './model';
+import routes from './routes';
+
+export default { model, routes };
