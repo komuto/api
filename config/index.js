e201d00c34370ee6e97dfd64255773d4d306edbf
Andrew Junior <andrew@pionize.com>
2017-07-04 09:29:14 +0700

    add cache prefix

--- config/index.js
+++ config/index.js
@@ -67,8 +67,9 @@ def.imageUrl = 'http://188.166.246.46/uploads';
 
 // cache expired
 def.cache = {};
+def.cache.prefix = 'komuto-api';
 def.cache.debug = true;
-def.cache.enable = false;
+def.cache.enable = true;
 def.cache.duration = 1800000; // in ms: 30 minutes
 
 cfg.resolveLocalConfig(__dirname, (err, file) => {
