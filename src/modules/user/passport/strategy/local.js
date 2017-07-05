88e58a18a70cf3a365248ef7a04ac162fba734d2
syakuronabdan <abdan.s@students.amikom.ac.id>
2017-07-05 09:44:09 +0700

    Fix inconsistent err response

--- src/modules/user/passport/strategy/local.js
+++ src/modules/user/passport/strategy/local.js
@@ -2,6 +2,9 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import { User } from '../../model';
 import { BadRequestError } from '../../../../../common/errors';
 import { loginMsg, emailMsg } from '../../message';
+import { utils } from '../../../core';
+
+const { formatSingularErr } = utils;
 
 export default new LocalStrategy({
   usernameField: 'email',
@@ -9,12 +12,12 @@ export default new LocalStrategy({
   passReqToCallback: false,
 }, (email, password, done) => {
   new User({ email_users: email }).fetch().then((user) => {
-    if (!user) return done(new BadRequestError(loginMsg.title, emailMsg.not_found), false);
+    if (!user) return done(new BadRequestError(loginMsg.title, formatSingularErr('email', emailMsg.not_found)), false);
     return user.checkPasswordFromApi(password)
       .then((body) => {
         body = JSON.parse(body);
         if (!body.data) {
-          return done(new BadRequestError(loginMsg.title, loginMsg.wrong_password), false);
+          return done(new BadRequestError(loginMsg.title, formatSingularErr('password', loginMsg.wrong_password)), false);
         }
         return done(null, user.toJSON());
       });
