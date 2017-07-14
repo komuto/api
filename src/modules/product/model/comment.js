c03bb389844414b19b1dd504c199fbf74436dd71
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-07-14 13:38:47 +0700

    List comments order by created_at.

--- src/modules/product/model/comment.js
+++ src/modules/product/model/comment.js
@@ -48,6 +48,7 @@ class CommentModel extends bookshelf.Model {
    */
   static async getByDiscussionId(id, page, pageSize) {
     return await this.where({ id_diskusi: id })
+      .orderBy('tgl_subdiskusi')
       .fetchPage({ page, pageSize, withRelated: ['user'] })
       .catch(() => {
         throw new BadRequestError('No Discussion Found');
