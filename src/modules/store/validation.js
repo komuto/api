6066bee3f5e088397e2e13ef7c04adfc02cfce10
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-07-17 16:21:26 +0700

    Update store validation.

--- src/modules/store/validation.js
+++ src/modules/store/validation.js
@@ -1,27 +1,20 @@
 const constraints = {};
 
 constraints.create = {
-  store: {
-    presence: true,
-  },
-  'store.name': {
-    presence: true,
-  },
-  'store.slogan': {
-    presence: true,
-  },
-  'store.description': {
-    presence: true,
-  },
-  expedition_services: {
-    presence: true,
-  },
-  user: {
-    presence: true,
-  },
-  address: {
-    presence: true,
-  },
+  store: { presence: true },
+  'store.name': { presence: true },
+  'store.slogan': { presence: true },
+  'store.description': { presence: true },
+  expedition_services: { presence: true },
+  user: { presence: true },
+  address: { presence: true },
+  'address.province_id': { presence: true },
+  'address.district_id': { presence: true },
+  'address.sub_district_id': { presence: true },
+  'address.village_id': { presence: true },
+  'address.postal_code': { presence: true },
+  'address.email': { presence: true },
+  'address.address': { presence: true },
 };
 
 constraints.createMessage = {
