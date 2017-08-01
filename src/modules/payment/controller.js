12d1a22da229cc4bad77c618aea345c659742e66
Satrio Wisnugroho <wisnugro@apples-iMac.local>
2017-08-01 08:34:37 +0700

    Update.

--- src/modules/payment/controller.js
+++ src/modules/payment/controller.js
@@ -1,4 +1,5 @@
 import doku from 'doku_library';
+import sha1 from 'sha1';
 
 const helper = doku.helper();
 const library = doku.library();
@@ -9,33 +10,33 @@ export const PaymentController = {};
 export default { PaymentController };
 
 PaymentController.store = async (req, res, next) => {
-  const sessionId = helper.sha1(helper.getTimes14Character());
+  // const sessionId = helper.sha1(helper.getTimes14Character());
   const shareKey = '3iVKd220tSAz';
-  const generateWords = library.doCreateWords(req, req.body.MALLID, shareKey);
-  const basket = {
-    name: 'Powerbank Xiaomi',
-    amount: '300000.00',
-    quantity: '1',
-    subtotal: '300000.00',
-  };
-
-  const dataPayment = {
-    req_mall_id: req.body.MALLID,
-    req_chain_merchant: 'NA',
-    req_amount: '10000.00',
-    req_words: generateWords,
-    req_purchase_amount: '10000.00',
-    req_trans_id_merchant: req.body.INVOICE,
-    req_request_date_time: helper.getTimes14Character(),
-    req_currency: '360',
-    req_purchase_currency: '360',
-    req_session_id: sessionId,
-    req_name: 'Satrio Wisnugroho',
-    req_payment_channel: 15,
-    req_basket: req.body.BASKET,
-    req_email: 'wisnu@skyshi.com',
-    req_token_id: 'asd',
-  };
+  // const generateWords = library.doCreateWords(req, req.body.MALLID, shareKey);
+  // const basket = {
+  //   name: 'Powerbank Xiaomi',
+  //   amount: '300000.00',
+  //   quantity: '1',
+  //   subtotal: '300000.00',
+  // };
+  //
+  // const dataPayment = {
+  //   req_mall_id: req.body.MALLID,
+  //   req_chain_merchant: 'NA',
+  //   req_amount: '10000.00',
+  //   req_words: generateWords,
+  //   req_purchase_amount: '10000.00',
+  //   req_trans_id_merchant: req.body.INVOICE,
+  //   req_request_date_time: helper.getTimes14Character(),
+  //   req_currency: '360',
+  //   req_purchase_currency: '360',
+  //   req_session_id: sessionId,
+  //   req_name: 'Satrio Wisnugroho',
+  //   req_payment_channel: 15,
+  //   req_basket: req.body.BASKET,
+  //   req_email: 'wisnu@skyshi.com',
+  //   req_token_id: 'asd',
+  // };
 
   // {
   //   "MALLID": "4533",
@@ -67,7 +68,22 @@ PaymentController.store = async (req, res, next) => {
   //   "TOKEN": "asd"
   // }
 
-  api.doPrePayment(dataPayment, (response) => {
+  // api.doPrePayment(dataPayment, (response) => {
+  //   const obj = JSON.parse(JSON.stringify(response));
+  //   if (obj.res_response_msg === 'SUCCESS' && obj.res_response_code === '0000') {
+  //     // SUCCESS RESPONSE
+  //   } else {
+  //     // FAILED RESPONSE
+  //   }
+  //   console.log(obj);
+  // });
+  //
+  // console.log(sessionId, shareKey, generateWords);
+
+  // const words = sha1(req.body.AMOUNT + req.body.MALLID + shareKey + req.body.INVOICE);
+  // req.resData = { data: words };
+
+  api.doPrePayment({ req_basket: library.formatBasket('testing item,10000.00,1,10000.00') }, (response) => {
     const obj = JSON.parse(JSON.stringify(response));
     if (obj.res_response_msg === 'SUCCESS' && obj.res_response_code === '0000') {
       // SUCCESS RESPONSE
@@ -77,6 +93,5 @@ PaymentController.store = async (req, res, next) => {
     console.log(obj);
   });
 
-  console.log(sessionId, shareKey, generateWords);
   return next();
 };
