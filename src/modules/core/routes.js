13ad2051099ac4df55da8759641f26305d306129
Satrio Wisnugroho <wisnu@skyshi.com>
2017-08-11 13:54:53 +0700

    Remove subdomain packages.

--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -3,6 +3,6 @@ import express from 'express';
 const routes = express.Router();
 
 routes.get('/status', (req, res) => res.status(200).json({ status: 'ok' }));
-routes.get('/_sub/foo/', (req, res) => res.json({ code: 200, status: true, message: 'komuto api' }));
+routes.get('/', (req, res) => res.json({ code: 200, status: true, message: 'komuto api' }));
 
 export default routes;
