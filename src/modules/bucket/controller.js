8ad517bdaa90748091d0579202b49f4a0052161f
Satrio Wisnugroho <wisnu@skyshi.com>
2017-09-29 15:43:12 +0700

    Min product discount.

--- src/modules/bucket/controller.js
+++ src/modules/bucket/controller.js
@@ -97,7 +97,8 @@ BucketController.saveCart = async (bucket, body, product, item, where) => {
     note: body.note,
     additional_cost: 0, // admin cost
     weight: product.weight * body.qty,
-    total_price: (product.price * body.qty) + delivery.cost + insuranceCost,
+    total_price: (product.price * product.discount ? product.discount : 1 * body.qty)
+    + delivery.cost + insuranceCost,
   });
   return await Item.updateInsert(where, _.assign(itemObj, where));
 };
