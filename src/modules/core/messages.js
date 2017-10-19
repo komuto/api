986114963786375637c7de934c82fcca5fc0e49c
Satrio Wisnugroho <wisnu@skyshi.com>
2017-10-19 15:25:10 +0700

    Add messages file.

---
+++ src/modules/core/messages.js
@@ -0,0 +1,31 @@
+export default {
+  unauthorized: {
+    msg: 'Please Login to use this feature',
+    code: 401,
+  },
+  path_not_found: {
+    msg: 'Path not found',
+    code: 404,
+  },
+  success: {
+    msg: 'Success',
+    code: 200,
+  },
+  database_down: {
+    msg: 'Database down',
+    code: 500,
+  },
+  bad_request: {
+    format: 'Invalid format',
+    parameter: 'Invalid parameter',
+    code: 400,
+  },
+  something_wrong: {
+    msg: 'Something went wrong',
+    code: 400,
+  },
+  marketplace_not_found: {
+    msg: 'Marketplace Not Found',
+    code: 404,
+  },
+};
