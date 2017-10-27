fafd59169e9e366c9f4751ebb6adfaf52a4bfb34
Satrio Wisnugroho <wisnu@skyshi.com>
2017-10-27 16:19:18 +0700

    Update response

--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -3,6 +3,6 @@ import express from 'express';
 const routes = express.Router();
 
 routes.get('/status', (req, res) => res.status(200).json({ status: 'ok' }));
-routes.get('/', (req, res) => res.json({ code: 200, status: true, message: 'komuto api' }));
+routes.get('/', (req, res) => res.json({ code: 200, status: true, message: `${req.marketplace.name} API` }));
 
 export default routes;
