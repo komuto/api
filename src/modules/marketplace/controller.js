1fa2fb2b842df25ed2311c1ff4eb520b5c8c7d9c
Satrio Wisnugroho <wisnu@skyshi.com>
2017-11-07 15:29:24 +0700

    Add product id to list banner response.

--- src/modules/marketplace/controller.js
+++ src/modules/marketplace/controller.js
@@ -14,7 +14,25 @@ MarketplaceController.get = async (req, res, next) => {
 };
 
 MarketplaceController.getBanners = async (req, res, next) => {
-  const banners = await Banner.findByMarketplaceId(req.marketplace.id);
+  let banners = await Banner.findByMarketplaceId(req.marketplace.id);
+
+  banners = banners.map((banner) => {
+    banner = banner.serialize();
+    if (banner.link.includes('/detail/')) {
+      const sArr = banner.link.split('.');
+      const pArr = sArr[sArr.length - 2].split('-');
+      const storeId = sArr[sArr.length - 1];
+      const productId = pArr[pArr.length - 1];
+      return {
+        ...banner,
+        product_id: `${productId}.${storeId}`,
+      };
+    }
+    return {
+      ...banner,
+      product_id: null,
+    };
+  });
 
   req.resData = {
     message: 'Banners Data',
