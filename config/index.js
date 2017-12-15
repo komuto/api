022b1c5ea24eba144316e708303f498c10a2d808
Satrio Wisnugroho <wisnu@skyshi.com>
2017-12-15 23:07:03 +0700

    Setting environment

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: '../.env' });
 const path = require('path');
 const cfg = require('../common/config');
 
@@ -5,7 +6,6 @@ const def = {};
 
 // setup default env
 def.env = process.env.NODE_ENV || 'development';
-process.env.NODE_ENV = def.env;
 
 def.debug = true;
 def.https = false;
